ce1ef4e91cc4a659e851a03fc390e313df4f1b89
helen121027 <helen121027@gmail.com>
2023-06-07 23:05:01 +0900

    Feat : create 페이지 레이아웃

---
+++ src/pages/CreateList.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const CreateList = () => {
+  const navigate = useNavigate();
+  const [ title, setTitle ] = useState('');
+  const [ content, setContent ] = useState('');
+
+  const toMainPage = () => {
+    navigate('/')
+  }
+  console.log(title)
+  const handleAddList = () => {
+
+  }
+
+  return(
+    <div className="flex h-screen justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold mb-[50px]">할 일 추가</h1>
+        <div className="w-full flex justify-end">
+          <button className="w-[130px] h-[40px] bg-main text-white text-2xl rounded-sm mt-10" onClick={toMainPage}>목록</button>
+        </div>
+        <div className="my-10">
+          <form>
+            <p className="text-xl text-main font-bold">제목</p>
+            <input 
+              id="title"
+              type="text"
+              onChange={(e) => {setTitle(e.target.value)}}
+              className="border-main border-2 w-[700px] h-[40px] my-5 required"
+            />
+            <p className="text-main font-bold text-xl">내용</p>
+            <textarea 
+              rows="8"
+              className="border-main border-2 w-[700px] min-h-[60px] my-5 required resize-none" 
+            >
+            </textarea>
+          </form>
+        </div>
+        <button className="bg-main text-white text-2xl rounded-sm w-[130px] h-[40px]">추가</button>
+      </div>
+    </div>
+  )
+}
+export default CreateList;
