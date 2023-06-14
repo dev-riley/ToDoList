520e80b2294b4167ec11f454a646d046ec518716
helen121027 <helen121027@gmail.com>
2023-06-14 17:27:00 +0900

    Chore: tailwindCSS 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       main: '#00498C',
       'red-dark': '#8B0000',
       black: colors.black,
-      white: colors.white
+      white: colors.white,
+      slate: colors.slate,
     },
     extend: {},
   },
