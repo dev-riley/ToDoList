3ce1962c0c6de57eacff18bc41bbe446e1835bf6
helen121027 <helen121027@gmail.com>
2023-06-14 17:55:34 +0900

    Feat: item 삭제 기능

--- src/components/Card/CardItem.jsx
+++ src/components/Card/CardItem.jsx
@@ -1,8 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
+import { deleteItem } from "../../api/axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const CardItem = ({item}) => {
   const { id, title, content, state, createdTime } = item
+  const navigate = useNavigate();
+
+  const deleteButton = (id) => {
+    deleteItem(id)
+      .then(() => {
+        console.log('삭제 성공!')
+        navigate('/')
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
   return ( 
     <div className="flex justify-center">
       <div className="bg-slate-200 w-[330px] m-1 p-3">
@@ -14,8 +30,12 @@ const CardItem = ({item}) => {
           {content}
         </div>
         <div className="flex justify-end space-x-3">
-          <FontAwesomeIcon icon={faPenToSquare} style={{color: '#00498C', height: '20px'}}/>
-          <FontAwesomeIcon icon={faTrashCan} style={{color: '#8B0000', height: '20px'}}/>
+          <button>
+            <FontAwesomeIcon icon={faPenToSquare} style={{color: '#00498C', height: '20px'}}/>
+          </button>
+          <button onClick={() => deleteButton(id)}>
+            <FontAwesomeIcon icon={faTrashCan} style={{color: '#8B0000', height: '20px'}}/>
+          </button>
         </div>
       </div>
     </div>
