2bf75ad7cf1062d51b9be20b68204550ec7ac5d5
helen121027 <helen121027@gmail.com>
2023-06-15 17:25:51 +0900

    Feat : 리스트 목록 조회, 등록, 삭제 api

---
+++ src/api/axios.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+// 전체 티켓 목록 조회하기
+export const getList = async () => {
+  return await axios({
+    method: 'GET',
+    url: 'http://localhost:8000/list'
+  })
+}
+
+// 티켓 등록하기
+export const createItem = async (data) => {
+  return await axios({
+    method: 'POST',
+    url: 'http://localhost:8000/list',
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    data: data
+  })
+}
+
+// 티켓 삭제하기 
+export const deleteItem = async (id) => {
+  return await axios({
+    method: 'DELETE',
+    url: `http://localhost:8000/list/${id}`,
+    headers: {
+      "Content-Type" : "application/json"
+    }
+  })
+}
